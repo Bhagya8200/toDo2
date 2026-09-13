17a30d03430d1c2a46be1a9bbfc31468f2cb3837
Bhagya8200 <neelapubhagyasree8200@gmail.com>
2026-09-13 16:52:33 +0530

    changed env variables

--- frontend/todo/src/App.tsx
+++ frontend/todo/src/App.tsx
@@ -12,7 +12,7 @@ import { TaskPriority, MoodType } from "../src/shared/types";
 // Create Apollo client
 const client = new ApolloClient({
   // uri: "http://localhost:4002/graphql",
-  uri: import.meta.env.VITE_API_URL || "https://to-do-backend-mqjd.onrender.com/graphql",
+  uri: "https://to-do-backend-mqjd.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
